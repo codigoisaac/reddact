c619d2d4f1d14cd1498b03a81177ccfcf3f33357
Isaac Muniz <codigoisaac@protonmail.com>
2022-02-10 21:21:12 -0300

    Icons for post stats

--- src/PostList.js
+++ src/PostList.js
@@ -2,6 +2,11 @@ import { useState } from "react";
 import TimeAgo from "timeago-react";
 import * as timeago from "timeago.js";
 import pt_BR from "timeago.js/lib/lang/pt_BR";
+import {
+  ChatAltIcon,
+  ArrowCircleUpIcon,
+  StarIcon,
+} from "@heroicons/react/outline";
 timeago.register("pt_BR", pt_BR);
 
 const PostList = (props) => {
@@ -126,17 +131,23 @@ const PostBadge = (props) => {
 const PostStats = (props) => {
   return (
     <div className="mt-[.2rem] text-xs">
-      {props.awards >= 1 && <Stat title="Prêmios" score={props.awards} />}
-      <Stat title="Ups" score={props.ups} />
-      <Stat title="Comentários" score={props.comments} />
+      {props.awards >= 1 && (
+        <Stat
+          icon={<StarIcon className="text-_yellow" />}
+          score={props.awards}
+        />
+      )}
+      <Stat icon={<ArrowCircleUpIcon />} score={props.ups} />
+      <Stat icon={<ChatAltIcon />} score={props.comments} />
     </div>
   );
 };
 
 const Stat = (props) => {
   return (
-    <div className="inline-block mr-3 text-gray-600 dark:text-gray-500">
-      {props.score} {props.title}
+    <div className="mr-3 text-gray-600 dark:text-gray-500 inline-flex">
+      <span className="w-[1rem] mr-1">{props.icon}</span>
+      <span>{props.score}</span>
     </div>
   );
 };
